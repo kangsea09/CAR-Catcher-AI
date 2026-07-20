7908fe4a8a4ef7489db05120ca59ad6b5c887879
강세아 <gangsea817@gmail.com>
2026-07-20 18:54:00 +0900

    init

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,16 @@
 // router.tsx
 import { createBrowserRouter } from "react-router";
-// import { AppLayout } from "./layout";
+import { AppLayout } from "./layout";
+import Main from "./pages/Main";
 
 export const Router = createBrowserRouter([
   {
-    // element: <AppLayout />,
+    element: <AppLayout />,
     errorElement: <div>페이지를 불러오는 중 오류가 발생했습니다.</div>,
     children: [
       {
         path: "/",
-        // element: <MonitoringPage data={mockMonitoringData} />,
+        element: <Main />,
       },
     ],
   },
